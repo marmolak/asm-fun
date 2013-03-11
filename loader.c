ba8d97a718d9ede66ab9c168743f62a1748fb645
Robin Hack <rhack@redhat.com>
2013-03-11 09:14:01 +0100

    Old (propably unused) debug code was removed.

--- loader.c
+++ loader.c
@@ -111,25 +111,6 @@ void child_work (void)
 		exit (EXIT_FAILURE);
 	}
 
-
-#ifdef DEBUG
-	/* just set registers and exit with code 2 - must pass! */
-	/* const char *const runcode = "\x5e\x48\x31\xc0\x48\x05\x3c\x00\x00\x00\x48\x31\xff\x66\xff\xc7\x66\xff\xc7\x0f\x05"; */
-
-	/* print hell and end with exit code 2 */
-	const char *const runcode = "\x5e\x66\x81\xc6\x30\x00\x48\x31\xc0\xfe\xc0\x48\x31\xff\x66\xff\xc7\x48\x31\xd2\x66\x81\xc2\x05\x00\x0f\x05\x48\xb8\x3c\x00\x00\x00\x00\x00\x00\x00\x48\x31\xff\x66\xff\xc7\x66\xff\xc7\x0f\x05\x48\x65\x6c\x6c\x0a";
-	memcpy ((code + 1), runcode, 53);
-#else
-	/* read shell code */
-	const int bsize = area_size - loader_len;
-	/* code + 1 == skip 8 bytes */
-	int rt = read (STDIN_FILENO, (code + 1), bsize);
-	if ( rt < 0 ) {
-		printf ("I cant read!\n");
-		exit (EXIT_FAILURE);
-	}
-#endif
-
 	prepare_castle (code, stack);
 	loader (code, stack);
 
@@ -165,26 +146,6 @@ int main (void)
 #ifdef DEBUG
 	/* real debugging */
 	child_work ();
-	exit (EXIT_SUCCESS);
-
- 	/* Code only for examination */
-	/* allocate memory for code and stack */
-	long *const code = mmap (NULL, 1024, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_PRIVATE |  MAP_ANONYMOUS, 0, 0);
-	if ( code == MAP_FAILED ) {
-		perror ("mmap failed");
-		exit (EXIT_FAILURE);
-	}
-	char *const stack = mmap (NULL, 1024, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_PRIVATE |  MAP_ANONYMOUS, 0, 0);
-	if ( stack == MAP_FAILED ) {
-		perror ("mmap failed");
-		exit (EXIT_FAILURE);
-	}
-
-	const char *const runcode = "\x66\x83\xc6\x28\x48\x31\xc0\xfe\xc0\x48\x31\xff\x66\xff\xc7\x48\x31\xd2\x66\x83\xc2\x05\x0f\x05\xb8\x3c\x00\x00\x00\x48\x31\xff\x66\xff\xc7\x66\xff\xc7\x0f\x05\x48\x65\x6c\x6c\x0a";
-
-	memcpy (code, runcode, 46);
-	another_loader (code, stack);
-
 	exit (EXIT_SUCCESS);
 #endif
 
